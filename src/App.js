fb24e6587573476d1e59f79edec960d7a1072fcd
Dayaldonadkar <Dayalpython@gmail.com>
2023-07-28 17:12:44 +0530

    image is responsive

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <img
-        className="w-full h-[50%]"
+        className="w-full lg:w-[70%] xl:w-[50%] 2xl:w-[60%] h-[40%] md:h-[60%] 2xl:h-[70%]"
         src={under}
         alt="Download Under Construction Png Animation Clipart St - Under Construction Png Animation@nicepng.com"
       />
