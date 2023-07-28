b36a56af7c7c2324871c4430f4702ddf157161bb
Dayaldonadkar <Dayalpython@gmail.com>
2023-07-28 17:04:51 +0530

    image and favicon added,tite changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import under from "./Assets/Images/underConstruction.jpg";
 
 const App = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      Reviveing of this website is in progress
+      <img
+        className="w-full h-[50%]"
+        src={under}
+        alt="Download Under Construction Png Animation Clipart St - Under Construction Png Animation@nicepng.com"
+      />
     </div>
   );
 };
